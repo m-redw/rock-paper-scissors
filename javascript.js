967e38c6510e18b952e1cf6c1c6164af2f3cf526
Edwin <264506916+m-redw@users.noreply.github.com>
2026-05-16 11:41:54 -0700

    Make one playable round of rock paper scissors
    Make 3 functions to get player choice, computer choice,
    and a play function that takes in those two choices as input.
    Inside play function, check player choice and match it with computer
    choice to see who wins.
    
    Output is print winner or tie.
    
    Winners have their score incremented, but no use yet.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,75 @@
-console.log("Hello World")
+let humanScore = 0;
+let computerScore = 0;
+
+
+function getComputerChoice() {
+    let rand = Math.random() * 30 + 1;
+
+    if (rand <= 10) {
+        return "rock";
+    } else if (rand <= 20) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    let choice = prompt("Rock, paper, or scissors? Choose!");
+    return choice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice == "rock") {
+        switch (computerChoice) {
+            case "rock": 
+                console.log("Tie.");
+                break;
+            case "scissors": 
+                console.log("You win! Rock beats Scissors.");
+                humanScore++;
+                break;
+            case "paper":
+                console.log("You lose! Paper beats Rock.");
+                computerScore++;
+                break;
+        }
+    } else if (humanChoice == "scissors") {
+        switch (computerChoice) {
+            case "rock": 
+                console.log("You lose! Rock beats Scissors.");
+                computerScore++;
+                break;
+            case "scissors": 
+                console.log("Tie.");
+                break;
+            case "paper":
+                console.log("You win! Scissors beats Paper.");
+                humanScore++;
+                break;
+        }
+    } else if (humanChoice == "paper")  {
+        switch (computerChoice) {
+            case "rock": 
+                console.log("You win! Paper beats Rock.");
+                humanScore++;
+                break;
+            case "scissors": 
+                console.log("You lose! Scissors beats Paper.");
+                computerScore++;
+                break;
+            case "paper":
+                console.log("Tie.");
+                break;
+        }
+    } else {
+        console.log("You didn't pick Rock, Paper, or Scissors...");
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
